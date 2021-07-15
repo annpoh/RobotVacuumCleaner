03b882b228f67d318c86b3b37a57dfc6781c64e7
Anna Pohl Lundgren <annlu532@student.liu.se>
2021-07-15 15:59:22 +0200

    Simple test of DC motor driver

--- pymakr/main.py
+++ pymakr/main.py
@@ -1,50 +1,33 @@
 import pycom
 import time
+from machine import Pin
 import machine
 import lib.rvc as rvc
 import lib.keys as keys
 
 pycom.heartbeat(False)
 pycom.rgbled(0x00FFFF)
-
+"""
 if not pybytes.is_connected():
     print("Not connected to pybytes. Updating config file:")
     pybytes.set_config('wifi', {'ssid': keys.SSID, 'password' : keys.PASSWORD})
     print("Trying to connect...")
     pybytes.connect()
-
-wlan = WLAN(mode=WLAN.STA)
-while not wlan.isconnected():
-    pycom.rgbled(0xFF0000)
-    print('Initializing connection attempt...')
-    wlan.connect(ssid=keys.SSID, auth=(WLAN.WPA2, keys.PASSWORD))
-    if wlan.isconnected():
-        print("Connected!")
-        pycom.rgbled(0x00FFFF)
-        break
-    else:
-        print("Failed connecting to network. Next attempt in 5 seconds.")
-    pycom.rgbled(0xFF0000)
-    time.sleep(5)
     
 # Sync Real Time Clock
 rtc = machine.RTC()
 rtc.ntp_sync(keys.NTP_SERVER)
 
-# Init routines
-# setState('parked')
-# initUART
-# notifyStarted()
-#robot = rvc.RobotVacuumCleaner()
+"""
 time.sleep(5)
 
-#alarm = machine.Timer.Alarm(handler=self.collectSensorData, ms=5000, arg=None, periodic=True)
-sreg = (1,0,0,0)
+en12 = Pin('P19', mode=Pin.OUT)
+pinA1 = Pin('P20', mode=Pin.OUT)
+pinA2 = Pin('P21', mode=Pin.OUT)
 
-temp = (0,0,0,0)
-while True:
-    time.sleep(5)
-    step()
+en12(0)
+pinA1(1)
+pinA2(0)
 
 
 while True: #Forever loop
@@ -57,5 +40,3 @@ while True: #Forever loop
 
     pycom.rgbled(0x00FF00)  # Green
     time.sleep(1)
-
-    #print(f"Time library says: {time.time()} or {time.localtime()}")
